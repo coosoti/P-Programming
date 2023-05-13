a97d11e47840283c6a5bfff5aad832cc5e3fa11d
coosoti <charlesosoti@gmail.com>
2023-05-13 10:47:33 +0300

    module and if __name__

---
+++ python-basics/module.py
@@ -0,0 +1,11 @@
+# my_script.py
+
+def hello(name):
+    return f"Hello, {name}."
+
+if __name__ == "__main__":
+    # This code will only be executed if the script is run as a standalone program.
+    name  = "Charles"
+    print(hello(name))
+
+
