de2e9e0c958bdd00f3adcdb09748b18676469493
coosoti <charlesosoti@gmail.com>
2023-06-08 13:53:46 +0300

    DocstringS

---
+++ docstrings.py
@@ -0,0 +1,18 @@
+def calculate_square(number):
+    """
+    Calculates the square of a given number.
+
+    Args:
+        number (int or float): The input number.
+
+    Returns:
+        float: The square of the input number.
+
+    Examples:
+        >>> calculate_square(5)
+        25.0
+        >>> calculate_square(2.5)
+        6.25
+    """
+    return number ** 2
+
